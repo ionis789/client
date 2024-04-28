463d00625fe7539738b4051846774f426712f5bf
ionis789 <ionionis789@gmail.com>
2024-04-28 23:48:17 +0300

    fixing routing on site render.com

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,17 +1,17 @@
 import "./S.module.css";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DialogsPage from "../DialogsPage/DialogsPage.jsx";
 import WelcomePageContainer from "../WelcomePage/WelcomePageContainer.jsx";
 
 const App = () => {
   return (
     <div className={"h-screen "}>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path={"/"} element={<WelcomePageContainer />} />
-          <Route path={"/d/*"} element={<DialogsPage />} />
+          <Route path={"/d"} element={<DialogsPage />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 };
